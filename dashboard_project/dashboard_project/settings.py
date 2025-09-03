16c99c3a3015defca3d4ae340f9626135f3db075
Guillermo1804 <144832378+Guillermo1804@users.noreply.github.com>
2025-09-03 01:02:39 -0600

    Update settings.py

--- dashboard_project/dashboard_project/settings.py
+++ dashboard_project/dashboard_project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,5 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
 
 
