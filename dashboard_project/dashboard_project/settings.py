ba412743f8ba0062621845f402cfc7a12fc5abde
Guillermo1804 <144832378+Guillermo1804@users.noreply.github.com>
2025-09-03 01:29:52 -0600

    Update settings.py

--- dashboard_project/dashboard_project/settings.py
+++ dashboard_project/dashboard_project/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/static/"STATIC_URL = 'static/'
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -130,6 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
 
 
 
